65a982763d6a1eae2a1585b9ea8072befe494d21
s.carswell@unimelb.edu.au <s.carswell@unimelb.edu.au>
2020-10-27 05:07:49 +0000

    Saved version

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -6,7 +6,7 @@ class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-
+# comment
     # Any code you write here will run when the form opens.
 
 
